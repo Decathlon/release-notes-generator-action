26e5d52b8a184a2a408315d3ee58addd7ef305b2
Marco Mornati <marco.mornati@decathlon.com>
2020-12-15 17:24:13 +0100

    fix: added missing owner id

--- entrypoint.sh
+++ entrypoint.sh
@@ -59,7 +59,7 @@ fi
 if [[ "$ACTION" == "$TRIGGER_ACTION" ]]; then
     echo "Creating release notes for Milestone $MILESTONE_NUMBER into the $OUTPUT_FILENAME file"
     java -jar /github-release-notes-generator.jar \
-    --changelog.repository=${REPOSITORY_NAME} \
+    --changelog.repository=${OWNER_ID}/${REPOSITORY_NAME} \
     --changelog.github.username=${GH_USERNAME} \
     --changelog.github.password=${GITHUB_TOKEN} \
     --spring.config.location=${CONFIG_FILE} \
